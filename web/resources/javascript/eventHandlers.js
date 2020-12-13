b5f7a13aa1d8dd07626dc8180e08aa7a6f6f4cdc
LeTrongDat <heijihattori249@gmail.com>
2020-12-14 05:28:13 +0700

    Minor refact when determine mouse-coor

--- web/resources/javascript/eventHandlers.js
+++ web/resources/javascript/eventHandlers.js
@@ -5,7 +5,8 @@ document.getElementById("Layer_1").onmousedown = (e) => {
         rErr.innerText = "Radius is required";
         return;
     } else rErr.innerText = "";
-    let [x, y] = [(e.offsetX - 150) / 30, -(e.offsetY - 150) / 30];
+    let rect = e.target.getBoundingClientRect();
+    let [x, y] = [(e.clientX - rect.left - 150) / 30, -(e.clientY - rect.top - 150) / 30];
     let itX = document.querySelector("input[type=hidden]").nextElementSibling;
     let itY = itX.nextElementSibling;
     itX.value = x.toFixed(3);
